e4be91085aa736ce0fffd7e7829fbeb6048bc943
RIKY HOSTING <muhammadraskina@gmail.com>
2024-10-07 19:56:26 +0800

    Add files via upload

---
+++ vynaa.js
@@ -0,0 +1,54 @@
+const settings = {
+    headerImageDesktop: "https://telegra.ph/file/880aaef1599624e37ac22.jpg", // Background image for desktop
+    headerImageMobile: "https://telegra.ph/file/880aaef1599624e37ac22.jpg", // Background image for mobile
+    profilePicture: "https://qu.ax/ZumRd.jpg",
+    profileName: "@AlwaysrikyDev",
+    socialMedia: {
+        x: {
+            url: "https://discord.gg/c6wYDZfhhc",
+            followers: "1.2k"
+        },
+        facebook: {
+            url: "https://www.instagram.com/alwaysriky_devreal01",
+            followers: "1.6k"
+        },
+        youtube: {
+            url: "https://www.youtube.com/@Alwaysrikyreal",
+            followers: "10"
+        },
+        instagram: {
+            url: "https://instagram.com/alwaysriky_devreal01",
+            followers: "1.6k"
+        },
+        tiktok: {
+            url: "https://www.tiktok.com/@alwaysrikydev",
+            followers: "1k"
+        },
+        telegram: {
+            url: "https://t.me/Alwaysrikyreal",
+            followers: "1k"
+        }
+    },
+    albums: [
+        {
+            title: "Album 1",
+            description: "My first album",
+            coverImage: "https://telegra.ph/file/4bc5749bbdf19606b8b7a.jpg",
+            link: "https://open.spotify.com/album/abc"
+        },
+        {
+            title: "Album 2",
+            description: "My second album",
+            coverImage: "https://telegra.ph/file/31c26d4a7ec6dbaf6b891.jpg",
+            link: "https://open.spotify.com/album/def"
+        },
+        {
+            title: "Album 3",
+            description: "My third album",
+            coverImage: "https://telegra.ph/file/9578806504a73366fc6b3.jpg",
+            link: "https://open.spotify.com/album/ghi"
+        }
+    ],
+    whatsappChatUrl: "https://wa.me/6282312934025" // WhatsApp chat URL
+};
+          
